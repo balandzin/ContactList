a22111b3a1741568b1cafe01e9af8573e31c65aa
balandzin <balandzin2602@gmail.com>
2023-12-14 19:40:34 +0300

    модифицировал логику

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Person {
-    let name: String?
+    let name: String
     let surname: String
     let telephone: String
     let email: String
@@ -17,14 +17,14 @@ struct Person {
         var contacts:[Person] = []
         let dataStore = DataStore()
         
-        let mixedNames = dataStore.names.shuffled()
-        let mixedSurnames = dataStore.surnames.shuffled()
-        let mixedTelephones = dataStore.telephones.shuffled()
-        let mixedEmails = dataStore.emails.shuffled()
+        let mixedNames = Array(Set(dataStore.names))
+        let mixedSurnames = Array(Set(dataStore.surnames))
+        let mixedTelephones = Array(Set(dataStore.telephones))
+        let mixedEmails = Array(Set(dataStore.emails))
         
         if !dataStore.names.isEmpty {
             for (index, _) in mixedNames.enumerated() {
-                if mixedSurnames.count > index && mixedEmails.count > index && mixedTelephones.count > index {
+                if mixedSurnames.count > index && mixedEmails.count > index && mixedTelephones.count > index { // если в массивах разное количество элементов, то число элементов итогового массива будет равно минимальному
                     
                     contacts.append(
                         Person(
